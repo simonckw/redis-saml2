66775a5ef6d57e467b91ad7e72dbec8cfd593a6d
Simon Wong <simonckwong@outlook.com>
2022-06-04 10:40:04 +0800

    Comment out @EnableRedisHttpSession as it is not needed

--- src/main/java/com/sckw/spring/redis/saml2/config/RedisSessionConfig.java
+++ src/main/java/com/sckw/spring/redis/saml2/config/RedisSessionConfig.java
@@ -1,10 +1,10 @@
 package com.sckw.spring.redis.saml2.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
-@EnableRedisHttpSession
+// RedisHttpSession is enabled as long as spring-session-data-redis is included
+// @EnableRedisHttpSession
 public class RedisSessionConfig {
 
 }
